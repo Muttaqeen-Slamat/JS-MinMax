29ff737737fd56fcd4b716429488bb0764e65627
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 12:32:17 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,15 @@
+let numbers = [0,2,4,6,8,10]
+
+function max(){
+    let maxNumb = Math.max(...numbers)
+    console.log(maxNumb);
+}
+max()
+
+
+function min(){
+    let minNumb = Math.min(...numbers)
+    console.log(minNumb);
+}
+min()
+
