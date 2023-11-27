d31ce8aef2dc2cdaf4f94acddcdcc7c9153ef958
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 12:35:32 +0200

    patch 1

--- script/code.js
+++ script/code.js
@@ -1,12 +1,14 @@
+//global array
 let numbers = [0,2,4,6,8,10]
 
+//function for max number of the array and making use of rest operator
 function max(){
     let maxNumb = Math.max(...numbers)
     console.log(maxNumb);
 }
 max()
 
-
+//function for min number of the array and making use of rest operator
 function min(){
     let minNumb = Math.min(...numbers)
     console.log(minNumb);
